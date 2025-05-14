6ad452c2fdfacb7155b42349bfa37dd292ecebcf
AnkitRPi <ankit@rpi>
2025-05-14 21:45:35 +0530

    Refactored device info

--- cam_unit.py
+++ cam_unit.py
@@ -1,40 +1,34 @@
 import time
+import os
 import communicator
 
 import constants
-
-LOCATION_HOME = (28.4241,77.0358)
-LOCATION_SPAZE = (28.4203,77.0402)
-LOCATION_CLUB = (28.4264,77.0335)
-LOCATIONS = [LOCATION_HOME, LOCATION_SPAZE, LOCATION_CLUB]
-DEVICE_CAM = "DEVICE_CAM"
+import device_info
 
 def get_device_id_str():
     file = open(constants.RPI_SERIAL_FILENAME, "r")
     content = file.read()
-    serial_id_str = content.rstrip()
-    if len(serial_id_str) < 10 or len(serial_id_str) > 20:
+    serial_id_str = content.rstrip().lstrip().rstrip(os.linesep)[:-1] # TODO last char still remains @!!!!@#E@$@$@
+    if len(serial_id_str) != 16:
         print(f"Incorrect serial ID : {content} in {constants.RPI_SERIAL_FILENAME}")
         quit(-1)
-    print(f"device_id_str = {serial_id_str}")
     return serial_id_str
 
 class CamUnit:
-    def __init__(self, comm, device_id_str):
-        self.network_id = -1
-        self.device_id = device_id_str
-        self.latlng = LOCATION_HOME
-        self.neighbourhood = None
-        self.min_hops_from_command_central = -1
+    def __init__(self, dinfo, comm):
+        self.dinfo = dinfo
         self.comm = comm
+        # TODO Topology, or should this go in device info
+        # self.neighbourhood = None
+        # self.min_hops_from_command_central = -1
 
     def is_registered(self):
         return self.network_id > 0
 
     def register_myself(self):
-        (self.network_id, self.neighbourhood) = self.comm.register_unit(self.lat, self.lng)
-        print ("Registered device with ID %d at location %s,%s" % (self.network_id, self.lat, self.lng))
-        print ("It's neighbourhood looks like : %s" % self.neighbourhood)
+        print (f"Registered device with ID {self.dinfo.device_id} at location {self.dinfo.gps}")
+        self.comm.register(self.dinfo)
+        # TODO Handle callback to complete registration
 
     def get_picture(self):
         pass
@@ -45,9 +39,10 @@ class CamUnit:
         pass
 
 def main():
-    get_device_id_str()
-    comm = communicator.Communicator(DEVICE_CAM, get_device_id_str())
-    unit = CamUnit(comm)
+    device_id_str = get_device_id_str()
+    dinfo = device_info.DeviceInfo(device_id_str)
+    comm = communicator.Communicator()
+    unit = CamUnit(dinfo, comm)
     unit.register_myself()
     # TODO wait for response asynchronously
     unit.send_heartbeat()
