9f3ae892daa727448db7bb766e3a2d30d007e1d5
AnkitRPi <ankit@rpi>
2025-05-17 01:25:46 +0530

    Added file communicator Added mav listener Added basic central command

---
+++ central.py
@@ -0,0 +1,24 @@
+import time
+import os
+
+class CommandCentral:
+    def __init__(self):
+        self.node_list = []
+
+    def init_listener(self):
+        read_from_file("network_sim.txt")
+        pass
+
+    def print_node_info(self, node):
+        print(node)
+
+    def print_map(self):
+        for n in self.node_list:
+            self.print_node_info(n)
+
+def main():
+    print(f" === Welcome to Central Command ====")
+
+if __name__=="__main__":
+    main()
+
--- communicator.py
+++ communicator.py
@@ -1,7 +1,10 @@
+import time
 import constants
 import device_info
 
-from vyomcloudbridge.services import queue_writer_json
+#from vyomcloudbridge.services import queue_writer_json
+#from vyomcloudbridge.services import vyom_listener
+#from vyomcloudbridge.listeners import mav_listener
 
 class Communicator:
     def __init__(self):
@@ -32,10 +35,20 @@ class Communicator:
         return []
 
 def main():
-    dinfo = device_info.DeviceInfo("9eccfdf8c851a5ef")
-    comm = Communicator()
-    comm.register(dinfo)
-    comm.send_heartbeat(5)
+    #vl = vyom_listener.VyomListener()
+    vl = mav_listener.MavListener()
+    try:
+        vl.start()
+        while vl.is_running:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        vl.stop()
+    #dinfo = device_info.DeviceInfo("9eccfdf8c851a5ef")
+    #comm = Communicator()
+    #comm.register(dinfo)
+    #comm.send_heartbeat(5)
 
 if __name__=="__main__":
     main()
