563cb4f77d8d6952c0273c61fdf2e1bd04b9e781
AnkitRPi <ankit@rpi>
2025-05-16 10:55:56 +0530

    Camera working now, was some issue with sleep after start. Renaming camunit to devnode

--- camera.py
+++ camera.py
@@ -7,23 +7,23 @@ class Camera:
     def __init__(self, dinfo, o_dir="/tmp/camera_captures"):
         self.dinfo = dinfo
         self.output_dir = o_dir
+        self.cam = Picamera2()
+        self.cam.start()
+        time.sleep(2)
 
     def start(self):
+        print(f"Making outout directory {self.output_dir}")
         os.makedirs(self.output_dir, exist_ok=True)
 
     def take_picture(self):
         ts_ns = time.time_ns()
         fname = f"{self.output_dir}/capture_{self.dinfo.device_id_str}_{ts_ns}.jpg"
-        print(f"Will write image to file : {fname}")
-        with Picamera2() as cam:
-            cam.configure(cam.create_still_configuration())
-            cam.start()
-            time.sleep(2)
-            try:
-                cam.capture_file(fname)
-            except Exception as e:
-                print("Couldn't take picture")
-                print(e)
+        print(f" ==== Will write image to file : {fname}")
+        try:
+            self.cam.capture_file(fname)
+        except Exception as e:
+            print("Couldn't take picture")
+            print(e)
             return ""
         print(f"... Written image to file : {fname}")
         return fname
