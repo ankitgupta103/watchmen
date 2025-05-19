5125700e216f7986d93d2bf45a770ffa54efe70a
AnkitRPi <ankit@rpi>
2025-05-19 14:12:50 +0530

    Fixing a bug in transmiting message

--- file_communicator.py
+++ file_communicator.py
@@ -46,43 +46,42 @@ class FileCommunicator:
         message_type = data['message_type']
         message_id = data['message_id']
         last_sender = data['last_sender']
+        if not self.simulated_layout.is_neighbour(last_sender, self.dinfo.device_id_str):
+            # print("I did not see this because it isnt in range in reality")
+            return
+        if last_sender == self.dinfo.device_id_str:
+            # print("Self Message, Skipping")
+            return
+        if message_type != constants.MESSAGE_TYPE_HEARTBEAT:
+            print("Skipping non HB message")
+            return
         if message_id in self.messages_processed:
             #print("Skipping already processed message")
             return
-        if not self.simulated_layout.is_neighbour(last_sender, self.dinfo.device_id_str):
-            # print("I did not see this because it isnt in range in reality")
-            self.messages_processed.append(message_id)
-            return
+        self.messages_processed.append(message_id)
         if last_sender != self.dinfo.device_id_str and last_sender not in self.neighbours_seen:
-            print(f"I saw a new neighbour : {self.dinfo.device_id_str} : {last_sender}")
+            #print(f"I saw a new neighbour : {self.dinfo.device_id_str} : {last_sender}")
             self.neighbours_seen.append(last_sender)
-        if message_type != constants.MESSAGE_TYPE_HEARTBEAT:
-            print("Skipping non HB message")
-            self.messages_processed.append(message_id)
-            return
-        if last_sender == self.dinfo.device_id_str:
-            #print("Self Message, Skipping")
-            self.messages_processed.append(message_id)
-            return
         path_so_far = data['path_so_far']
         if self.dinfo.device_id_str in path_so_far:
-            print(f"Cyclic Message, Skipping : {path_so_far}")
-            self.messages_processed.append(message_id)
+            print(f"{self.dinfo.device_id_str} : Cyclic Message, Skipping : {path_so_far}")
             return
         hb_id = data['hb_id']
         hb_ts = data['hb_ts']
-        network_ts = data['network_ts']
-        neighbours = data["neighbours"]
+        hb_network_ts = data['network_ts']
+        hb_neighbours = data["neighbours"]
 
         # If desired_path_1,2 not empty and I am not on desired path, skip
         # else (if desired_path is empty OR I am on desired_path)
 
         path_so_far.append(self.dinfo.device_id_str)
+        if hb_id == "SSS":
+            print(f"{self.dinfo.device_id_str} : {path_so_far}")
         hb_msg = {
                 "message_type" : constants.MESSAGE_TYPE_HEARTBEAT,
                 "hb_id" : hb_id,
                 "hb_ts" : hb_ts,
-                "neighbours" : neighbours,
+                "neighbours" : hb_neighbours,
                 "desired_path1" : [],
                 "desired_path2" : [],
                 "path_so_far" : path_so_far,
@@ -91,8 +90,6 @@ class FileCommunicator:
                 "message_id" : message_id,
                 }
         self._write_json_to_file(hb_msg)
-        
-        self.messages_processed.append(message_id)
 
     def _listen_once(self):
         filenames = os.listdir(self.ndir)
@@ -146,7 +143,7 @@ def main():
     for j in range(10):
         for comm in comms:
             comm.send_heartbeat(time.time_ns())
-            time.sleep(1)
+            time.sleep(0.05)
         print(f"{j} rounds of HB done.")
         time.sleep(5)
     for comm in comms:
