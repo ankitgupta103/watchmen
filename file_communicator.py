102d0bd9479a1c6f0fc73758e7c83be1d33e8aa4
AnkitRPi <ankit@rpi>
2025-05-21 12:24:03 +0530

    Probable failure on network

--- file_communicator.py
+++ file_communicator.py
@@ -5,12 +5,14 @@ import constants
 import json
 import layout
 import glob
+import random
 
 class FileCommunicator:
     def __init__(self):
         # This is a simulated network layout, it is only used to "receive" messages which a real network can see.
         self.simulated_layout = layout.Layout()
         self.dev = {} # ID -> Obj
+        self.robustness = 0.99
 
     def add_dev(self, devid, devobj):
         self.dev[devid] = devobj
@@ -18,8 +20,12 @@ class FileCommunicator:
 
     # Send msg of mtype to dest, None=all neighbours (broadcast mode).
     def send_to_network(self, msg, devid, dest=None):
+        time.sleep(0.1)
+        if random.random() > self.robustness:
+            return False
         if dest is not None:
             self.dev[dest].process_msg(msg)
         else:
             for n in self.simulated_layout.get_neighbours(devid):
                 self.dev[n].process_msg(msg)
+        return True
