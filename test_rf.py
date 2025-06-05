4c48a8c396a2536e8891414421e7a457e203a18e
Ankit <ankitgupta@gmail.com>
2025-06-05 23:08:37 +0530

    test rf

--- test_rf.py
+++ test_rf.py
@@ -25,6 +25,7 @@ def setup():
     radio.listen = True
     radio.payloadSize = MAX_CHUNK_SIZE
     radio.setAutoAck(True)
+    radio.set_retries(10, 5)
 
 def keep_receiving_bg():
     num_messages = 0
@@ -33,10 +34,11 @@ def keep_receiving_bg():
         has_payload, pipe = radio.available_pipe()
         if has_payload:
             data = radio.read(MAX_CHUNK_SIZE)
+            num_messages += 1
             datastr = data.decode()
             print(f"{num_messages} Received data : {datastr}")
-            send_message(f"Ack:{datastr}")
-            num_messages += 1
+            if datastr.find("Ack") < 0:
+                send_message(f"Ack:{datastr}")
 
 def keep_reading():
     reader_thread = threading.Thread(target=keep_receiving_bg, daemon=True)
@@ -54,14 +56,14 @@ def send_message(msg):
     radio.listen = True
     t2 = time.time()
     print(f"Sending {succ} in time {(t2-t1)*1000} msec")
-    time.sleep(0.01)  # slight delay
+    time.sleep(1)  # slight delay
     return succ
 
 def send_messages():
     num_to_send = 100
     num_successfully_sent = 0
     for i in range(num_to_send):
-        succ = send_message(f"Message#{i}")
+        succ = send_message(f"M#{i}")
         if succ:
             num_successfully_sent += 1
     print(f"Num messages sent = {num_to_send}, success = {num_successfully_sent}")
@@ -72,7 +74,6 @@ def main():
     if sys.argv[1] == "r":
         time.sleep(1000)
     elif sys.argv[1] == "s":
-        radio.stopListening(b"test")
         send_messages()
     else:
         print("argv1 needs to be r OR s")
