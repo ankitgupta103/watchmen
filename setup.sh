88f7fec3aa29b6bd326da4382c781e94822001f5
calebjoshuapaul <calebjoshuapaul@gmail.com>
2025-06-10 14:35:20 +0530

    Update requirements and modify setup script for improved environment setup
    - Expanded requirements.txt to include a comprehensive list of dependencies, ensuring all necessary packages are specified for the project.
    - Modified setup.sh to create a Python virtual environment without system site packages, enhancing isolation and compatibility of the environment.

--- setup.sh
+++ setup.sh
@@ -4,18 +4,18 @@ set -e
 echo "Updating system..."
 sudo apt update
 
-echo "Installing system packages..."
-sudo apt install -y \
-    libcamera-apps \
-    libcamera-dev \
-    libcamera-tools \
-    python3-picamera2 \
-    python3-libcamera \
-    python3-venv \
-    python3-pip
+# echo "Installing system packages..."
+# sudo apt install -y \
+#     libcamera-apps \
+#     libcamera-dev \
+#     libcamera-tools \
+#     python3-picamera2 \
+#     python3-libcamera \
+#     python3-venv \
+#     python3-pip
 
 echo "Creating venv and installing Python packages..."
-python3 -m venv venv --system-site-packages
+python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
