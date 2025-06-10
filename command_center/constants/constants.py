83c7cb74fab7384126db7615eac4e2a6435ce936
Amardeep Saini <amrdeepsaini@gmail.com>
2025-06-10 12:19:53 +0530

    code for central service skeleton

---
+++ command_center/constants/constants.py
@@ -0,0 +1,5 @@
+import os
+
+package_name = "watchman"
+log_dir = "/var/log/watchman"
+log_file = os.path.join(log_dir, "watchman.log")
---
+++ command_center/services/data_rocessor.py
@@ -0,0 +1,51 @@
+import threading
+from command_center.utils.abc_service import AbsService
+
+
+class DataProcessor(AbsService):
+    def __init__(self):
+        self.image_in_process = set()
+        self.is_running = False
+
+    def start(self):
+        for image in self.image_in_process:
+            if image not in self.image_in_process:
+                self.image_in_process.add(image)
+
+
+    def start_image_processing(self):
+        while self.is_running:
+            for image in self.image_in_process:
+                if image not in self.image_in_process:
+                    self.image_in_process.add(image)
+
+#       If image not in image_in_process:
+#            add image to  self.image_in_process
+#            if image has corresponding json:
+#       	       event_found, event_detail = classiy_image(image)
+#                ff event_found:
+#                      success: publish_data_to_vyom(image, filename, event_detail)
+#                      If success:
+#                           delete image and json file both,
+#                           remove from the  self.image_in_process
+#                       else:
+#                            remove from self.image_in_process
+#                else:
+#                      delete image and json file both,
+#                      remove from the  self.image_in_process
+#             else:
+# 	         continue
+#        Else:
+# 	 continue: // image already in proccess
+
+    def start(self):
+        self.is_running = True
+        thread = threading.Thread(target=self.image_processing)
+        thread.start()
+        
+
+    def stop(self):
+        self.is_running = False
+    
+
+    
