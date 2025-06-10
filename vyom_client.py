a43e38b463887466891ade1d4e59c50cf1951594
Amardeep Saini <amrdeepsaini@gmail.com>
2025-06-10 19:17:40 +0530

    vyom listener added

--- vyom_client.py
+++ vyom_client.py
@@ -27,14 +27,18 @@ class VyomClient:
         self.expiration_time = 2000  # milisecond
 
     def on_image_arrive(
-        self, node_hn: str, image: bytes, timestamp: str = None, filename: str = None, severity: Union[int,str] = 1
+        self,
+        node_hn: str,
+        image: bytes,
+        event_id: str = None,
+        timestamp: str = None,
     ):
         """_summary_
         Args:
             node_hn (str): hostname of device
             image (bytes): image in bytes format
             timestamp (str, optional): timestamp string in iso format, in UTC timezone
-            filename (str, optional): filename of the image which will saved to s3 and fetch with that na,e
+            event_id (str, optional): event_id of the image which will saved to s3 and fetch with that na,e
         """
         try:
             if not node_hn in self.HN_TO_VYOM_ID:
@@ -42,12 +46,12 @@ class VyomClient:
                 return
             vyom_machine_id = self.HN_TO_VYOM_ID[node_hn]
 
-            if filename is None:
+            if event_id is None:
                 epoch_ms = int(time.time() * 1000)
                 filename = f"{epoch_ms}.jpg"
-            elif not filename.endswith(".jpg"):
-                filename = filename.split(".")[0]
-                filename = f"{filename}.jpg"
+            elif not event_id.endswith(".jpg"):
+                filename = event_id.split(".")[0]
+                filename = f"{event_id}.jpg"
 
             self.writer.write_message(
                 message_data=image,
@@ -62,52 +66,18 @@ class VyomClient:
         except Exception as e:
             print(f"Error setting location in VyomClient: {e}")
 
-    def on_image_bb_arrive(
-        self, node_hn: str, image: bytes, timestamp: str = None, filename: str = None
-    ):  # LIVE with priority 2
-        """_summary_
-
-        Args:
-            node_hn (str): hostname of device
-            image (bytes): image in bytes format
-            timestamp (str, optional): timestamp string in iso format, in UTC timezone
-            filename (str, optional): filename of the image which will saved to s3 and fetch with that na,e
-        """
-        try:
-            if not node_hn in self.HN_TO_VYOM_ID:
-                self.logger.error(f"Node {node_hn} not found in HN_TO_VYOM_ID")
-                return
-            vyom_machine_id = self.HN_TO_VYOM_ID[node_hn]
-
-            if filename is None:
-                epoch_ms = int(time.time() * 1000)
-                filename = f"{epoch_ms}.jpg"
-            elif not filename.endswith(".jpg"):
-                filename = filename.split(".")[0]
-                filename = f"{filename}.jpg"
-
-            self.writer.write_message(
-                message_data=image,
-                data_type="image",
-                data_source="IMAGE",
-                destination_ids=["s3"],
-                source_id=vyom_machine_id,
-                filename=filename,
-                priority=2,
-                merge_chunks=True,
-            )
-        except Exception as e:
-            print(f"Error setting location in VyomClient: {e}")
-
     def on_event_arrive(
-        self, node_hn: str, timestamp: str = None, filename: str = None
+        self,
+        node_hn: str,
+        event_id: str = None,
+        timestamp: str = None,
     ):
         """_summary_
 
         Args:
             node_hn (str): _description_
             timestamp (str, optional): _description_. Defaults to None.
-            filename (str, optional): _description_. Defaults to None.
+            event_id (str, optional): _description_. Defaults to None.
         """
         try:
             if not node_hn in self.HN_TO_VYOM_ID:
@@ -120,17 +90,32 @@ class VyomClient:
                 mission_id=default_mission_id,
                 data_source="IMAGE",
             )
+            try:
+                base_filename = event_id.split(".")[0]
+            except Exception as e:
+                base_filename = event_id
+
+            try:
+                file_ext = event_id.split(".")[1]
+            except Exception as e:
+                file_ext = "jpg"
+
+            filename1 = f"{base_filename}_c.{file_ext}"
+            filename2 = f"{base_filename}_f.{file_ext}"
+
             payload = {
-                "image_key": f"{file_s3_dir}/{filename}.jpg",
-                "image_crop_key": f"{file_s3_dir}/{filename}.jpg",
+                "image_c_key": f"{file_s3_dir}/{filename1}",
+                "image_f_key": f"{file_s3_dir}/{filename2}",
             }
+            epoch_ms = int(time.time() * 1000)
+            filename = f"{epoch_ms}.json"
             self.writer.write_message(
                 message_data=payload,
                 data_type="json",
                 data_source="EVENT",
                 destination_ids=["s3"],
                 source_id=vyom_machine_id,
-                filename=timestamp,
+                filename=filename,
                 priority=3,
                 expiry_time=self.expiration_time,
                 merge_chunks=True,
@@ -140,7 +125,7 @@ class VyomClient:
             print(f"Error setting location in VyomClient: {e}")
 
     def on_hb_arrive(
-        self, node_hn: str, timestamp: str = None, lat: int = None, long: int = None
+        self, node_hn: str, lat: int = None, long: int = None, timestamp: str = None
     ):
         """
         timestamp: str = None, lat: int=None, long: int=None
@@ -168,13 +153,17 @@ class VyomClient:
                 # "health": {"status": 1, "message": ""},
                 "health": None,
             }
+
+            epoch_ms = int(time.time() * 1000)
+            filename = f"{epoch_ms}.json"
+
             self.writer.write_message(
                 message_data=payload,
                 data_type="json",
                 data_source="machine_state",
                 destination_ids=["s3"],
                 source_id=vyom_machine_id,
-                filename=timestamp,
+                filename=filename,
                 priority=3,
                 expiry_time=self.expiration_time,
             )
@@ -202,20 +191,7 @@ if __name__ == "__main__":
             binary_data = response.content
             time.sleep(1)
             client.on_image_arrive(
-                node_hn="central", image=binary_data, filename="test_full.jpg"
-            )
-        else:
-            print(
-                f"[Error] Failed to download {image_url} (Status: {response.status_code})"
-            )
-
-        # test on_image_bb_arrive
-        image_url = "https://sample-videos.com/img/Sample-jpg-image-50kb.jpg"
-        response = requests.get(image_url)
-        if response.status_code == 200:
-            binary_data = response.content
-            client.on_image_bb_arrive(
-                node_hn="rpi2", image=binary_data, filename="test_bb.jpg"
+                node_hn="central", image=binary_data, event_id="test_full.jpg"
             )
         else:
             print(
@@ -223,7 +199,7 @@ if __name__ == "__main__":
             )
 
         # test on_event_arrive
-        client.on_event_arrive(node_hn="rpi3", filename="test_bb.jpg")
+        client.on_event_arrive(node_hn="rpi3", event_id="test_bb.jpg")
 
         # test on_hb_arrive
         client.on_hb_arrive(node_hn="rpi4", lat=75.66666, long=73.0589455)
