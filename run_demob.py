3edff1e0fa8e351682ce0b5c188c1e892b7a1a19
calebjoshuapaul <calebjoshuapaul@gmail.com>
2025-06-14 09:45:27 +0530

    Implement logging functionality in run_demob.py, replacing print statements with logger calls for improved debugging and monitoring.

--- run_demob.py
+++ run_demob.py
@@ -9,6 +9,7 @@ import socket
 import random
 from rf_comm import RFComm
 from vyom_client import VyomClient
+import logging
 
 import gps
 import constants
@@ -16,6 +17,16 @@ import constants
 ALLDIR = "../processed_images"
 CCDIR = "../command_images"
 CRITICAL_DIR = "../processed_images/critical"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler('run_demob.log'),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
 
 def get_hostname():
     return socket.gethostname()
@@ -33,7 +44,7 @@ def is_node_passthrough(devid):
 
 def get_next_dest(devid):
     idx = -1
-    print(f"Getting next dest for {devid}")
+    logger.info(f"Getting next dest for {devid}")
     num_nodes = len(constants.PATH_DEMOB)
     for i in range(num_nodes):
         if devid == constants.PATH_DEMOB[i]:
@@ -67,32 +78,33 @@ class CommandCenter:
         self.images_saved = []
         self.msgids_seen = []
         self.vyom_client = VyomClient()
+        self.logger = logger
 
     def print_status(self):
         while True:
-            print("######### Command Center printing status ##############")
+            self.logger.info("######### Command Center printing status ##############")
             for x in self.node_map.keys():
-                print(f" ####### {x} : {self.node_map[x]}")
+                self.logger.info(f" ####### {x} : {self.node_map[x]}")
             for x in self.images_saved:
-                print(f"Saved image : {x}")
-            print("#######################################################")
+                self.logger.info(f"Saved image : {x}")
+            self.logger.info("#######################################################")
             time.sleep(10)
 
     def process_image(self, msgstr):
         try:
             orig_msg = json.loads(msgstr)
         except Exception as e:
-            print(f"Error loadig json {e}")
-        print("Checking for image")
+            self.logger.error(f"Error loadig json {e}")
+        self.logger.info("Checking for image")
         if "i_d" in orig_msg:
-            print("Seems like an image")
+            self.logger.info("Seems like an image")
             imf = orig_msg["i_f"]
             ims = orig_msg["i_s"]
             imstr = orig_msg["i_d"]
             evid = orig_msg["e_i"]
             im = image.imstrtoimage(imstr)
             fname = f"{CCDIR}/{ims}_{random.randint(1000,2000)}_{imf}"
-            print(f"Saving image to {fname}")
+            self.logger.info(f"Saving image to {fname}")
             im.save(fname)
             self.images_saved.append(fname)
             # im.show()
@@ -104,14 +116,14 @@ class CommandCenter:
                 image_bytes = image.imstrtobytes(imstr)
                 self.vyom_client.on_image_arrive(node_hn=ims, image=image_bytes, filename=filename)
             except Exception as e:
-                print(f"Error sending image to vyom client {e}")
+                self.logger.error(f"Error sending image to vyom client {e}")
 
 
     # A:1205:100:12
     def process_hb(self, hbstr):
         parts = hbstr.split(':')
         if len(parts) != 4:
-            print(f"Error parsing hb : {hbstr}")
+            self.logger.error(f"Error parsing hb : {hbstr}")
             return
         nodeid = parts[0]
         hbtime = parts[1]
@@ -137,25 +149,25 @@ class CommandCenter:
                     lat = float(lat)
                     long = float(long)
                 except Exception as e:
-                    print(f"Error parsing gpsloc {e}, gpsloc={gpsloc}")
+                    self.logger.error(f"Error parsing gpsloc {e}, gpsloc={gpsloc}")
                     lat = None
                     long = None
             self.vyom_client.on_hb_arrive(node_hn=nodeid, lat=lat, long=long)
         except Exception as e:
-            print(f"Error sending hb to vyom client {e}")
+            self.logger.error(f"Error sending hb to vyom client {e}")
     
     # A:1205
     def process_event(self, eventstr):
         parts = eventstr.split(':')
         if len(parts) != 3:
-            print(f"Error parsing event message : {eventstr}")
+            self.logger.error(f"Error parsing event message : {eventstr}")
             return
         nodeid = parts[0]
         eventtime = parts[1]
         evid = parts[2]
         if nodeid not in self.node_map:
             self.node_map[nodeid] = (0, "", "", 1, 1, [eventtime])
-            print(f"Wierd that node {nodeid} not in map yet")
+            self.logger.warning(f"Wierd that node {nodeid} not in map yet")
             return
         (hbcount, hbtime, gpsloc, photos_taken, events_seen, event_ts_list) = self.node_map[nodeid]
         event_ts_list.append(eventtime)
@@ -164,12 +176,12 @@ class CommandCenter:
         try:
             self.vyom_client.on_event_arrive(node_hn=nodeid, event_id=evid)
         except Exception as e:
-            print(f"Error sending event to vyom client {e}")
+            self.logger.error(f"Error sending event to vyom client {e}")
 
     def process_gps(self, msgstr):
         parts = msgstr.split(':')
         if len(parts) != 2:
-            print(f"Error parsing event message : {msgstr}")
+            self.logger.error(f"Error parsing event message : {msgstr}")
             return
         nodeid = parts[0]
         gpsloc = parts[1]
@@ -182,19 +194,19 @@ class CommandCenter:
         if msgid not in self.msgids_seen:
             self.msgids_seen.append(msgid)
         else:
-            print(f"Skipping message id : {msgid}")
+            self.logger.info(f"Skipping message id : {msgid}")
             return
         if mst == constants.MESSAGE_TYPE_PHOTO:
-            print(f"########## Image receive at command center")
+            self.logger.info(f"########## Image receive at command center")
             self.process_image(msgstr)
         elif mst == constants.MESSAGE_TYPE_HEARTBEAT:
-            print(f"########## Messsage receive at command center : {mst} : {msgstr}")
+            self.logger.info(f"########## Messsage receive at command center : {mst} : {msgstr}")
             self.process_hb(msgstr)
         elif mst == constants.MESSAGE_TYPE_EVENT:
-            print(f"########## Messsage receive at command center : {mst} : {msgstr}")
+            self.logger.info(f"########## Messsage receive at command center : {mst} : {msgstr}")
             self.process_event(msgstr)
         elif mst == constants.MESSAGE_TYPE_GPS:
-            print(f"########## Messsage receive at command center : {mst} : {msgstr}")
+            self.logger.info(f"########## Messsage receive at command center : {mst} : {msgstr}")
             self.process_gps(msgstr)
         return True
 
@@ -212,25 +224,26 @@ class DevUnit:
         self.photos_taken = 0
         self.critical_images_processed = []
         self.critical_images_sent = []
+        self.logger = logger
 
     def process_msg(self, msgid, mst, msgstr):
         if msgid not in self.msgids_seen:
             self.msgids_seen.append(msgid)
         else:
-            print(f"Skipping message id : {msgid}")
+            self.logger.info(f"Skipping message id : {msgid}")
             return
         if is_node_passthrough(self.devid):
             next_dest = get_next_dest(self.devid)
-            print(f"Next dest for {self.devid} is {next_dest}")
+            self.logger.info(f"Next dest for {self.devid} is {next_dest}")
             if next_dest == None:
-                print(f"{self.devid} Weird no dest for {self.devid}")
+                self.logger.warning(f"{self.devid} Weird no dest for {self.devid}")
                 return
-            print(f"In Passthrough mode, trying to aquire lock")
+            self.logger.info(f"In Passthrough mode, trying to aquire lock")
             with self.msg_queue_lock:
-                print(f"{self.devid} Adding message to send queue for {next_dest}")
+                self.logger.info(f"{self.devid} Adding message to send queue for {next_dest}")
                 self.msg_queue.append((msgstr, mst, next_dest))
         if is_node_src(self.devid):
-            print(f"{self.devid}: Src should not be getting any messages")
+            self.logger.warning(f"{self.devid}: Src should not be getting any messages")
 
     def get_images_to_send(self, critical_images):
         cropped = None
@@ -249,9 +262,9 @@ class DevUnit:
 
     def send_img(self, imgfile, evid):
         next_dest = get_next_dest(self.devid)
-        print(f"Sending image {imgfile} to {next_dest}")
+        self.logger.info(f"Sending image {imgfile} to {next_dest}")
         if next_dest == None:
-            print(f"{self.devid} Weird no dest for {self.devid}")
+            self.logger.warning(f"{self.devid} Weird no dest for {self.devid}")
             return
         mst = constants.MESSAGE_TYPE_PHOTO
         fname = imgfile.split("/").pop()
@@ -274,7 +287,7 @@ class DevUnit:
                     (msgstr, mst, dest) = self.msg_queue.pop(0)
                     to_send = True
             if to_send:
-                print(f"Propagating message {mst} to {dest}")
+                self.logger.info(f"Propagating message {mst} to {dest}")
                 self.rf.send_message(msgstr, mst, dest)
             time.sleep(0.5)
 
@@ -301,13 +314,13 @@ class DevUnit:
         events_seen = 0
         while True:
             (photos_seen, critical_images) = get_files_in_dir(ALLDIR, CRITICAL_DIR)
-            print(f"Taken sofar={self.photos_taken}, now={photos_seen}")
-            print(f"Critical sofar={len(self.critical_images_processed)}, now={len(critical_images)}")
+            self.logger.info(f"Taken sofar={self.photos_taken}, now={photos_seen}")
+            self.logger.info(f"Critical sofar={len(self.critical_images_processed)}, now={len(critical_images)}")
             self.photos_taken = photos_seen
             if len(critical_images) > 0:
                 (evid, cropped, full) = self.get_images_to_send(critical_images)
                 if cropped or full:
-                    print(f"{cropped}, {full}")
+                    self.logger.info(f"{cropped}, {full}")
                     events_seen += 1
                     self.send_event(evid)
                     time.sleep(2)
@@ -350,7 +363,7 @@ def run_unit():
     if hname not in constants.HN_ID:
         return None
     devid = constants.HN_ID[hname]
-    print(f"### Running as host {hname} and devid {devid}")
+    logger.info(f"### Running as host {hname} and devid {devid}")
     if is_node_dest(devid):
         cc = CommandCenter(devid)
         cc.print_status()
