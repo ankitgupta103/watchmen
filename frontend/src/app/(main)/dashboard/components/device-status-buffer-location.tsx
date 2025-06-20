ef55dc1acc71706d15420f79c89a836dc00abb8f
calebjoshuapaul <calebjoshuapaul@gmail.com>
2025-06-20 16:03:40 +0530

    Refactor DeviceListing and DeviceBuffer components for improved status display
    - Updated DeviceListing to include a Badge component for indicating machine online/offline status, enhancing user experience.
    - Refactored DeviceStatusBufferLocation to DeviceBuffer, simplifying its functionality by removing the online status display logic.
    - Improved code organization and readability by consolidating related functionalities.

--- frontend/src/app/(main)/dashboard/components/device-status-buffer-location.tsx
+++ frontend/src/app/(main)/dashboard/components/device-status-buffer-location.tsx
@@ -3,34 +3,15 @@
 import React from 'react';
 import { useMachineStats } from '@/hooks/use-machine-stats';
 
-import { Badge } from '@/components/ui/badge';
 import { TableCell } from '@/components/ui/table';
 
 import { formatBufferSize } from '@/lib/utils';
 
-export default function DeviceStatusBufferLocation({
-  machineId,
-}: {
-  machineId: number;
-}) {
-  const { data, buffer } = useMachineStats(machineId);
+export default function DeviceBuffer({ machineId }: { machineId: number }) {
+  const { buffer } = useMachineStats(machineId);
 
   return (
     <>
-      <TableCell>
-        <Badge
-          variant={
-            data !== null
-              ? 'default'
-              : data === null
-                ? 'destructive'
-                : 'secondary'
-          }
-          className="capitalize"
-        >
-          {data !== null ? 'Online' : 'Offline'}
-        </Badge>
-      </TableCell>
       <TableCell>{formatBufferSize(buffer)}</TableCell>
     </>
   );
