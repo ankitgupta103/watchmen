ba97552edca6b754e1af183aadc4b2476ea3e944
Ankit <ankitgupta@gmail.com>
2025-07-03 11:54:02 +0530

    a

--- sx1262-HAT/loop_comm.py
+++ sx1262-HAT/loop_comm.py
@@ -31,24 +31,47 @@ node = sx126x.sx126x(
     relay=False
 )
 
-def send_message(seq_num):
-    payload = f"MSG-{seq_num}"
+def send_message(msgstr, dest):
+    if len(msgstr) > 225:
+        print(f"[NOT SENDING] Msg too long : {len(msgstr)} : {msgstr}")
+        return
+    payload = msgstr
     offset_freq = FREQ - 850
     data = (
-        bytes([peer_addr >> 8]) +
-        bytes([peer_addr & 0xFF]) +
+        bytes([dest >> 8]) +
+        bytes([dest & 0xFF]) +
         bytes([offset_freq]) +
         bytes([my_addr >> 8]) +
         bytes([my_addr & 0xFF]) +
         bytes([node.offset_freq]) +
         payload.encode()
     )
+    msgs_sent.append((payload, time.time()))
     node.send(data)
     print(f"[SENT ] {payload}")
 
+msgs_sent = []
+msgs_recd = []
+
+def ack_time(msg):
+    for (m, t) in msgs_recd:
+        if m == f"Ack:{msg}":
+            return t
+    return -1
+
+def print_status():
+    print(f"{hname} : {len(msgs_recd)} {sorted(msgs_recd)}")
+    print(f"Num messages sent = {len(msgs_sent)}")
+    for s,t in msgs_sent:
+        ackt = ack_time(s)
+        if ackt > 0:
+            print(f"Acktime for {s} = {ackt-t}")
+
 def send_messages(num_messages):
     for i in range(num_messages):
-        send_message(i)
+        msgstr = f"MSG-{i}"
+        msgs_sent.append((msg, time.time()))
+        send_message(msgstr, peer_addr)
         time.sleep(0.1)
 
 def radioreceive(rssideb=False):
@@ -59,7 +82,7 @@ def radioreceive(rssideb=False):
         #print("message is "+str(r_buff[3:-1]),end='\r\n')
         #print("receive message from node address with frequence\033[1;32m %d,%d.125MHz\033[0m"%((r_buff[0]<<8)+r_buff[1],r_buff[2]+node.start_freq),end='\r\n',flush = True)
         peer_addr = r_buff[0]<<8 + r_buff[1]
-        msgstr = str(r_buff[3:-1])
+        msgstr = (r_buff[3:-1]).decode()
         printstr = f"## Received ## ## From @{peer_addr} : Msg = {msgstr}"
         if rssideb and node.rssi:
             # print('\x1b[3A',end='\r')
@@ -72,6 +95,9 @@ def radioreceive(rssideb=False):
             #print('\x1b[2A',end='\r')
         t2 = time.time()
         printstr += f"  [time to read = {t2-t1}]"
+        msgs_recd.append((msgstr, time.time()))
+        if msgstr.find("Ack") < 0:
+            send_message(f"Ack:{msgstr}")
         print(printstr)
 
 def keep_receiving_bg():
