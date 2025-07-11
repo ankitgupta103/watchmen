c85add149100e6af7ce19183fc920939cc93428a
Ankit <ankitgupta@gmail.com>
2025-07-11 14:51:20 +0530

    a

--- push_event.sh
+++ push_event.sh
@@ -1,4 +1,4 @@
-rm ../processed_images/*.jpg
-rm ../processed_images/critical/*.jpg
+rm /home/vyom/processed_images/*.jpg
+rm /home/vyom/processed_images/critical/*.jpg
 #RID=`tr -dc A-Z < /dev/urandom | head -c 3`
-cp testdata/forest_man_2.jpg ../images/
+cp /home/vyom/watchmen/testdata/forest_man_2.jpg /home/vyom/images/
