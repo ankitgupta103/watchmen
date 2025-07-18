27f56e8303039fdb4aad51d893557567176b71ba
anand <anandmohan.amp@gmail.com>
2025-07-18 10:10:16 +0530

    init test for gps

---
+++ 3dr-radio/test/gps-test/gps.py
@@ -0,0 +1,20 @@
+from machine import UART
+import time
+
+# # UART(3) uses P4 (TX) and P5 (RX) on OpenMV RT1062
+# uart = UART(1, baudrate=9600, tx=Pin("P4"), rx=Pin("P5"))
+
+UART_PORT = 1
+UART_BAUDRATE = 9600
+
+uart = UART(UART_PORT, baudrate=UART_BAUDRATE, timeout=1000)
+uart.init(UART_BAUDRATE, bits=8, parity=None, stop=1)
+
+print("Listening to GPS...")
+
+while True:
+    if uart.any():
+        data = uart.readline()
+        if data:
+            print(data.decode('utf-8', 'ignore'))
+    time.sleep(0.1)
