a7e25aff670f6c892b93416c7545bfe4784a277f
Ankit <ankitgupta@gmail.com>
2025-07-28 10:05:52 +0530

    a

--- 3dr-radio/hybrid_encryption.py
+++ 3dr-radio/hybrid_encryption.py
@@ -4,25 +4,30 @@ import os
 import utime
 from rsa.key import newkeys
 from rsa.pkcs1 import encrypt, decrypt, sign, verify
+import rsa
 
 import random
-
-aes_key = None
-
-public_key = None
-private_key = None
 
 def setup_aes():
     aes_key = os.urandom(32)
     return aes_key
 
-setup_aes()
+def load_rsa():
+    private_bytes = b'-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEApj9jaQGz+zyT/xjrYzAdsEOdEF1ec7ruqtgNVAuylU6uJY/I\n+LeVSvaEY3Seu4L+x99SUkp+fq05Z1nMKqKaOpXgfvLudDrXzLOFITkl5Ecag19Q\nCyjQWxm0yB2x9t3mM+VjCDJ9+Lzp8ynzHFFD18EiwRb/IarYFfiYfGRfaMzQLNri\n6zKu1xT/e+/AMFX3U9D/V/qgo4/zvW+hyFQvB3snsHX6+3f/FIt/YrsnrHmAoO7e\nUmeFhbBPw67UJMuHJs8jOPbziro2LHbrPpp8hC/gHuiHK1ll2iop9mUXAg5uBSLD\neBZ7s1A2psNTS2wR0/nMTjETxnZ2fGiF2i1gqwIDAQABAoIBAEKhARyivBmjK8V/\nnUeBj0SHtLlMUoCbmPAL6zuV/Jruj8kqGWflXAZRSrn5kWyhka9Vh87HYG8wyeLs\nEHG9/YYhb3oxrvQSaU73XBH2r4MQJEYmuxPd5bO9V8EkdaD1Sj/eXZR5eBdqz3DP\njUn6H/CmzWEJ8HLz3+reWW7xY3PCi9DgZrOt40NCTzR7rLB4dnFv1hIQx9QE1fGr\n9yvPGzq2fekY3OrntQQM54pAsEZF5aLbyMHzw/CBdXG1nxs6uK8zMxn2owQBabF5\nmGi1pQoVBB5/igQudTqGTXC3W7RmSHBCPOePzXu5OXiOiJE1CN9fl5qxtMIuamwj\nbfkHWFECgYEA6NdTc7UQ30sQZnMnE06hbGdzvNV7yKBQ9+p4jCMRKAJSUer3abTL\nnvsMFZfkBpZPLXm6VbwApm2brhol393Rl+jxfEQ4ToRvaE6pVeJ4mxytqqFBUbUK\npWmaF8J9/Emej+/KLlVOAjT3fv6MBozzHzwZCeTIKZJNekKjlgDd2+0CgYEAtshv\n/RRxUCFbaiBwjlq5rgzqKL253oj18bjJgIH6HSpzvXfEzNk79fGYuBCBc/ECnmNn\npcpYY6mZzq3uE0g5vE66cfGM6KOddPhg+GF5DhqqyonC9jDji++QPzOuOFUe8cQS\n2F5nqapB0jViJ1b4GVJXoxB9cjnmKGQg9jL3C/cCgYEAmGfA/v6okY/fpz9+dzvD\nm2JHtnWCNXsCJJQ73XZil26VlXsYAP/PPDuU2Fl4bvtZzilcVxvczRL3kMkau2LE\n+wsFbdJ1jKdRCNRcMJQxX04xOnucdq/qzQTHUQAEWOuTNyG8lAFQM0+aJGzXGL6P\nsIU02m3+un9B6WHPE7NzhK0CgYA6T0kCmIHpiSqreXvOvfycHLyakKP57QFgwo1t\ntIlAwqk3mTysCOUK+a65kXJqtUkblCSdjCaUbKeHeo8HkbPxccAi12cXVBLIHPB6\nbEX9DN7NTBNpDIGaw6rlrqv2hpkfkWhdpAg35PuofqU4XZM6KL2SZJFQXk4hNogZ\nYnrTUwKBgEtMHa26G6zgit6V1dzrMTeiCn8aL/y4BFvB0WinoRDlJxmHzA3SCFxY\nuhXJ2icxTYZ695xadA/ot1DubGFi01q2jEMKHIOERJZcq8bG3QE6NDDmILJ7vxx+\nl+oGHyCnM/zJdSR7Rb1JOsJN1d8JET4R4r0ljh3fHiEN2UmocSWY\n-----END RSA PRIVATE KEY-----\n'
+    public_bytes = b'-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApj9jaQGz+zyT/xjrYzAd\nsEOdEF1ec7ruqtgNVAuylU6uJY/I+LeVSvaEY3Seu4L+x99SUkp+fq05Z1nMKqKa\nOpXgfvLudDrXzLOFITkl5Ecag19QCyjQWxm0yB2x9t3mM+VjCDJ9+Lzp8ynzHFFD\n18EiwRb/IarYFfiYfGRfaMzQLNri6zKu1xT/e+/AMFX3U9D/V/qgo4/zvW+hyFQv\nB3snsHX6+3f/FIt/YrsnrHmAoO7eUmeFhbBPw67UJMuHJs8jOPbziro2LHbrPpp8\nhC/gHuiHK1ll2iop9mUXAg5uBSLDeBZ7s1A2psNTS2wR0/nMTjETxnZ2fGiF2i1g\nqwIDAQAB\n-----END PUBLIC KEY-----\n'
+
+    priv_key = rsa.PrivateKey.load_pkcs1(private_bytes)
+    #priv_key = serialization.load_pem_private_key(private_bytes)
+    print(priv_key)
+
+load_rsa()
 
 def setup_rsa():
-    global private_key
-    global public_key
     print("\nGenerating RSA Keys...")
     (public_key, private_key) = newkeys(1024)
+    print(public_key)
+    print(private_key)
+    return (public_key, private_key)
 
 def pad(data):
     pad_len = 16 - (len(data) % 16)
@@ -32,11 +37,9 @@ def unpad(data):
     num_bytes_to_remove = 0 - int(data[-1])
     return data[:num_bytes_to_remove]
 
-def encrypt_aes(msgstr):
-    global aes_key
+def encrypt_aes(msg, aes_key):
     iv = os.urandom(16)
     aes = ucryptolib.aes(aes_key, 2, iv)  # mode 2 = CBC
-    msg = msgstr.encode()
     padded_data = pad(msg)
     encrypted_data = aes.encrypt(padded_data)
     return (iv, encrypted_data)
@@ -44,12 +47,26 @@ def encrypt_aes(msgstr):
 def decrypt_aes(encrypted_msg, iv, aes_key):
     aes = ucryptolib.aes(aes_key, 2, iv)  # mode 2 = CBC
     decrypted_msg = unpad(aes.decrypt(encrypted_msg))
-    return decrypted_msg.decode()
+    return decrypted_msg
 
-def encrypt_rsa(msgstr):
-    return encrypt(msg.encode(), public_key)
-def decrypt_rsa(msg):
+def encrypt_rsa(msg, public_key):
+    return encrypt(msg, public_key)
+def decrypt_rsa(msg, private_key):
     return decrypt(msg, private_key)
+
+def encrypt_hybrid(msg, public_key):
+    # AES key 32 bytes, IV 16bytes, Message encrypted.
+    aes_key = os.urandom(32)
+    (iv, msg_aes) = encrypt_aes(msg, aes_key)
+    iv_rsa = encrypt_rsa(iv, public_key)
+    aes_key_rsa = encrypt_rsa(aes_key, public_key)
+    print(f"{len(aes_key)} -> {len(aes_key_rsa)}")
+    print(f"{len(iv)} -> {len(iv_rsa)}")
+    print(f"{len(msg)} -> {len(msg_aes)}")
+    return aes_key_rsa + iv_rsa + msg_aes
+
+def decrypt_hybrid(msg, private_key):
+    return ""
 
 def get_rand(n):
     rstr = ""
@@ -57,26 +74,47 @@ def get_rand(n):
         rstr += chr(65+random.randint(0,25))
     return rstr
 
-def test_encryption():
+# Debugging only
+def test_encryption(n2, enctype):
     clock_start = utime.ticks_ms()
     t0 = utime.ticks_diff(utime.ticks_ms(), clock_start)
-    setup_rsa()
+    if enctype == "RSA" or enctype == "HYBRID":
+        public_key, private_key = setup_rsa()
+    elif enctype == "AES":
+        aes_key = setup_aes()
+    else:
+        print("WRONG INPUT")
+        return
     t1 = utime.ticks_diff(utime.ticks_ms(), clock_start)
     print(f"Setup key in time {t1-t0} mili seconds")
-    print(private_key)
-    print(public_key)
     lenstr = 1
-    for i in range(1,15):
+    for i in range(1,n2):
         t2 = utime.ticks_diff(utime.ticks_ms(), clock_start)
         teststr = get_rand(lenstr)
         t3 = utime.ticks_diff(utime.ticks_ms(), clock_start)
-        teststr_enc = encrypt_rsa(teststr)
+        if enctype == "RSA":
+            teststr_enc = encrypt_rsa(teststr.encode(), public_key)
+        elif enctype == "AES":
+            iv, teststr_enc = encrypt_aes(teststr.encode(), aes_key)
+        elif enctype == "HYBRID":
+            teststr_enc = encrypt_hybrid(teststr.encode(), public_key)
+        else:
+            return
         t4 = utime.ticks_diff(utime.ticks_ms(), clock_start)
-        teststr_decrypt = decrypt_rsa(teststr_enc)
+        if enctype == "RSA":
+            teststr_decrypt = decrypt_rsa(teststr_enc, private_key)
+        elif enctype == "AES":
+            teststr_decrypt = decrypt_aes(teststr_enc, iv, aes_key)
+        elif enctype == "HYBRID":
+            teststr_decrypt = decrypt_hybrid(teststr_enc, private_key)
+        else:
+            return
         t5 = utime.ticks_diff(utime.ticks_ms(), clock_start)
-        print(f"Encrypting {len(teststr)} to {len(teststr_enc)}, creation time = {t3-t2}, enc time = {t4-t3}, decrypt time = {t5-t4}")
-        if teststr != teststr_decrypt:
+        print(f"Encrypting {len(teststr)} to {len(teststr_enc)}, creation time = {t3-t2}, enc time = {t4-t3}, decrypt time = {t5-t4} : {teststr_enc}")
+        if teststr.encode() != teststr_decrypt:
             print(f"Strings DONT match {teststr} != {teststr_decrypt}")
         lenstr = lenstr*2
 
-test_encryption()
+#test_encryption(7, "HYBRID")
+#test_encryption(7, "AES")
+#test_encryption(7, "RSA")
