277307dc2f16832b3ed78a26f0a7346ffad526b1
anand <anandmohan.amp@gmail.com>
2025-07-28 16:50:28 +0530

    removed globally added key exponent

--- 3dr-radio/hybrid_encryption.py
+++ 3dr-radio/hybrid_encryption.py
@@ -1,17 +1,12 @@
-# import ucryptolib
+import ucryptolib
 import os
 
-import time as utime 
+import time as utime
 # from rsa.key import newkeys
 # from rsa.pkcs1 import encrypt, decrypt, sign, verify
-# import rsa
+import rsa
 
 import random
-
-# # --- Your RSA Keys ---
-# n = 17137440455843848950481875150631737022736154257065297701812475847978114399732264735927775853371799030685286724310264465564309364089427386220887191257573292508281307329069584113167081903408457599506928088154361712265501112905423351175920217555974170362922297976183581700680237046563837134255012986832036203123759951600299568186862408890553036610966636967628186560798496806787496514526440241699655625593229083632980910855617738004743818057471515437942887009896231377488967374719942108774184891163446147523843438432365782085999110091159141885308918643274317353722487770773019656768635161675703118510899416835513918095843
-# e = 65537
-# d = 12615187450009533620978944762377388180537105601776113600386640711872807314742567521481967293196449343689065526967973642556402896863851647061267082225904578337076996345214214052695721993166214809542895337852371516621520212855277762649799640745729311699016745064086310511404499379547113765777433297009134539523777624900684374282201690134310128956793708925693155974317133931947830198369788194144628762425198047535927171705756461275458808811907690456799727302086389080155967585204568108550975572684331157770719110820487517512655378445684085939335175115951599496571067753599254778933192794939648200859515974319007877278793
 
 # ---------- Helper function to RSA encryption ------------
 def mod_exp(base, exp, mod):
@@ -99,7 +94,7 @@ def decrypt_aes(encrypted_msg, iv, aes_key):
     return decrypted_msg
 
 
-def encrypt_rsa(msg: bytes, n, e) -> int:    
+def encrypt_rsa(msg: bytes, n, e) -> int:
     m = int.from_bytes(msg, 'big')
     if m >= n:
         raise ValueError("Message too large for this key size (max: 256 bytes)")
@@ -111,12 +106,12 @@ def decrypt_rsa(ciphertext: int, d, n) -> bytes:
     length = (get_bit_length(m) + 7) // 8
     return m.to_bytes(length, 'big')
 
-def encrypt_hybrid(msg, public_key):
+def encrypt_hybrid(msg, n, e):
     # AES key 32 bytes, IV 16bytes, Message encrypted.
     aes_key = os.urandom(32)
     (iv, msg_aes) = encrypt_aes(msg, aes_key)
-    iv_rsa = encrypt_rsa(iv, public_key)
-    aes_key_rsa = encrypt_rsa(aes_key, public_key)
+    iv_rsa = encrypt_rsa(iv, n, e)
+    aes_key_rsa = encrypt_rsa(aes_key, n, e)
     print(f"{len(aes_key)} -> {len(aes_key_rsa)}")
     print(f"{len(iv)} -> {len(iv_rsa)}")
     print(f"{len(msg)} -> {len(msg_aes)}")
@@ -157,7 +152,7 @@ def test_encryption(n2, enctype):
         elif enctype == "AES":
             iv, teststr_enc = encrypt_aes(teststr.encode(), aes_key)
         elif enctype == "HYBRID":
-            teststr_enc = encrypt_hybrid(teststr.encode(), public_key)
+            teststr_enc = encrypt_hybrid(teststr.encode(), n, e)
         else:
             return
         # t4 = utime.ticks_diff(utime.ticks_ms(), clock_start)
@@ -167,7 +162,7 @@ def test_encryption(n2, enctype):
         elif enctype == "AES":
             teststr_decrypt = decrypt_aes(teststr_enc, iv, aes_key)
         elif enctype == "HYBRID":
-            teststr_decrypt = decrypt_hybrid(teststr_enc, private_key)
+            teststr_decrypt = decrypt_hybrid(teststr_enc, d, n)
         else:
             return
         # t5 = utime.ticks_diff(utime.ticks_ms(), clock_start)
@@ -176,6 +171,6 @@ def test_encryption(n2, enctype):
             print(f"Strings DONT match {teststr} != {teststr_decrypt}")
         lenstr = lenstr*2
 
-#test_encryption(7, "HYBRID")
-#test_encryption(7, "AES")
+test_encryption(7, "HYBRID")
+test_encryption(7, "AES")
 test_encryption(7, "RSA")
