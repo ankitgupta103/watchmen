1780edc447766668eaf45a0e5f9117febd4fd18c
Anand Mohan <85077039+anand2532@users.noreply.github.com>
2025-08-01 09:42:36 +0530

    /3dr-radio/test/person-det-loop : added model info.

--- 3dr-radio/test/person-det-dist/dist.py
+++ 3dr-radio/test/person-det-dist/dist.py
@@ -95,13 +95,15 @@ def get_rand(n):
         rstr += chr(65+random.randint(0,25))
     return rstr
 
-def get_input_size_expected():
-    print(model.input_width())
-    print(model.input_height())
-    print(model.input_channels())
+def model_info():
+    print("Model loaded:", model)
+
+
+
+model_info()
 
 while True:
-    # person_detection_loop()
-    get_input_size_expected()
+    person_detection_loop()
+    
 
 
