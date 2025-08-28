093bb6ba4e0d0f52f2ba44ac6c015cad6204e83b
Ankit <ankitgupta@gmail.com>
2025-08-28 22:05:02 +0530

    a

---
+++ openmv/test/imdiff/imd.py
@@ -0,0 +1,38 @@
+import sys
+from PIL import Image
+
+def im2bytes(fname):
+    img = Image.open(fname)
+    img = img.convert("L")
+    width, height = img.size
+    print(f"Image width: {width} pixels")
+    print(f"Image height: {height} pixels")
+    pixel_bytes = img.tobytes()
+    print(f"Pillow bytelen = {len(pixel_bytes)}")
+    return pixel_bytes
+
+def diff_ims(f1, f2):
+    b1 = im2bytes(f1)
+    b2 = im2bytes(f2)
+    print(len(b1))
+    print(len(b2))
+    if len(b1) != len(b2):
+        print("Size mismatch")
+        return None
+    d = []
+    for x in range(len(b1)):
+        d.append(abs(b2[x] - b1[x]))
+    num_diff = 0
+    for x in d:
+        if x > 32:
+            num_diff += 1
+    d32 = float(num_diff*100/len(b1))
+    print(f"Num of more than 32 diffs is {d32}")
+    return d32
+
+def main():
+    ratio = diff_ims(sys.argv[1], sys.argv[2])
+    print(f"Diff : {ratio>2.0}")
+
+if __name__ == "__main__":
+    main()
