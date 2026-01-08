4cac2b761783a60fbc22d288f45ed1e76528a8b7
Amardeep Saini <amrdeepsaini@gmail.com>
2026-01-08 13:07:12 +0530

    node addredd updated

--- netrajaal/test/micropySX1262/netrjaal_clone_305033f/main.py
+++ netrajaal/test/micropySX1262/netrjaal_clone_305033f/main.py
@@ -167,9 +167,9 @@ if uid == b'e076465dd7193a09':
 #     if not DYNAMIC_SPATH:
 #         shortest_path_to_cc = [219]
 elif uid ==  b'e076465dd7194025':
-    my_addr = 221
+    my_addr = 219
     if not DYNAMIC_SPATH:
-        shortest_path_to_cc = [225, 219]
+        shortest_path_to_cc = []
 elif uid == b'e076465dd7090d1c':
     my_addr = 221
     if not DYNAMIC_SPATH:
